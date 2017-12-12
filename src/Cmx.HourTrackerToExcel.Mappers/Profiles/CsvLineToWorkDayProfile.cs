520f1416cf77778db6d028b954d7a7402c4b6183
mattuu <maciej@uliasz.com>
2017-12-12 21:08:08 +0000

    Reorganised solution

---
+++ src/Cmx.HourTrackerToExcel.Mappers/Profiles/CsvLineToWorkDayProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace Cmx.HourTrackerToExcel.Mappers.Profiles
+{
+    public class CsvLineToWorkDayProfile : Profile 
+    {
+
+    }
+}
