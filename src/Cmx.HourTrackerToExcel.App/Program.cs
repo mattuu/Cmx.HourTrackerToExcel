6e86abbe4d7db88e0b1b96606c2c0265b660e398
Matt Uliasz <matt.uliasz@jet2.com>
2017-12-18 16:13:27 +0000

    Progress on TimesheetCalculator

--- src/Cmx.HourTrackerToExcel.App/Program.cs
+++ src/Cmx.HourTrackerToExcel.App/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Cmx.HourTrackerToExcel.Import;
 using Cmx.HourTrackerToExcel.Mappers;
 using Cmx.HourTrackerToExcel.Models.Export;
+using Cmx.HourTrackerToExcel.Services;
 
 namespace Cmx.HourTrackerToExcel.App
 {
@@ -13,16 +15,33 @@ namespace Cmx.HourTrackerToExcel.App
         {
             var csvReader = new CsvDataReader();
             var path = Path.Combine(Environment.CurrentDirectory, "..", "..", "export.csv");
-            var mapper = AutoMapperConfiguration.GetConfiguredMapper(t => Activator.CreateInstance(t));
+            var mapper = AutoMapperConfiguration.GetConfiguredMapper(Activator.CreateInstance);
+
+            var timesheetCalculator = new TimesheetCalculator();
 
             using (var stream = File.OpenRead(path))
             {
                 var csvLines = csvReader.Read(stream);
 
-                foreach (var csvLine in csvLines)
+                var workDays = csvLines.Select(mapper.Map<WorkDay>);
+
+                //foreach (var csvLine in csvLines)
+                //{
+                //    var workDay = mapper.Map<WorkDay>(csvLine);
+                //    Render(workDay);
+
+                    
+                //}
+
+                var weeks = timesheetCalculator.Aggregate(workDays);
+
+                foreach (var week in weeks.Result)
                 {
-                    var workDay = mapper.Map<WorkDay>(csvLine);
-                    Render(workDay);
+                    Console.WriteLine("week");
+                    foreach (var workDay in week.WorkDays)
+                    {
+                        Console.WriteLine($"{workDay?.Date:d}");
+                    }
                 }
             }
 
--- src/Cmx.HourTrackerToExcel.Services/TimesheetCalculator.cs
+++ src/Cmx.HourTrackerToExcel.Services/TimesheetCalculator.cs
@@ -9,18 +9,18 @@ namespace Cmx.HourTrackerToExcel.Services
 {
     public class TimesheetCalculator
     {
-        public Task<IEnumerable<ITimesheetWeek>> Aggregate(IEnumerable<IWorkDay> workDays)
+        public Task<IEnumerable<ITimesheetWeek>> Aggregate(IEnumerable<IWorkDay> workDays, DayOfWeek dayOfWeek = DayOfWeek.Sunday)
         {
             return Task.Run(() =>
             {
                 var result = new HashSet<TimesheetWeek>();
                 var timesheetWeek = new TimesheetWeek();
 
-                foreach (var workDay in workDays)
+                foreach (var workDay in workDays.OrderBy(wd => wd.Date))
                 {
                     timesheetWeek.AddDay(workDay);
 
-                    if (workDay.Date.DayOfWeek == DayOfWeek.Sunday)
+                    if (workDay.Date.DayOfWeek == dayOfWeek)
                     {
                         result.Add(timesheetWeek);
                         timesheetWeek = new TimesheetWeek();
