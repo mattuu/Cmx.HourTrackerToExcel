875079e770053b8b61a03a33ff8fe906e3a4ff51
Matt Uliasz <matt.uliasz@jet2.com>
2019-04-15 16:27:48 +0100

    Completed file upload component;

--- src/cmx.hour-tracker-to-excel.web/src/components/file-upload/file-upload.tsx
+++ src/cmx.hour-tracker-to-excel.web/src/components/file-upload/file-upload.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as React from 'react';
 import Dropzone from 'react-dropzone';
 import './file-upload.css';
@@ -23,6 +24,16 @@ class FileUpload extends React.Component {
   private acceptedFiles(acceptedFiles: File[]) {
     // tslint:disable-next-line:no-console
     console.log(acceptedFiles);
+
+    const data = new FormData();
+    data.append('formFiles', acceptedFiles[0]);
+    data.append('name', acceptedFiles[0].name);
+    data.append('description', 'some value user types');
+    // '/files' is your node.js route that triggers our middleware
+    axios.post('http://localhost:41006/api/file', data).then(response => {
+      // tslint:disable-next-line:no-console
+      console.log(response); // do something with the response
+    });
   }
 }
 
