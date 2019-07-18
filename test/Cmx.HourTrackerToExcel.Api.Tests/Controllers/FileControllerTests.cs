61c58482ab854e62972b8d9a5d5121ffa2b2e2e7
Matt Uliasz <matt@codemagix.co.uk>
2019-07-18 16:12:18 +0100

    Added test project for API;

---
+++ test/Cmx.HourTrackerToExcel.Api.Tests/Controllers/FileControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Cmx.HourTrackerToExcel.TestUtils.Attributes;
+using Cmx.HourTrackerToExcel.Api.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shouldly;
+
+namespace Cmx.HourTrackerToExcel.Api.Tests
+{
+    public class FileControllerTests
+    {
+        [Theory, AutoMoqData]
+        public async Task Post_ShouldReturnCorrectResult(IFixture fixture, FileController sut)
+        {
+            // arrange
+            var files = fixture.Build<IFormFileCollection>().Create();
+
+            // act
+            var actual = await sut.Post(files);
+
+            // assert
+            actual.ShouldBeOfType<FileStreamResult>();
+        }
+    }
+}
